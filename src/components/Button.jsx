b67ae9e13ca04ebf985ea9659223416dfbe6c916
Matheus Gomes <79264192+matheusngs@users.noreply.github.com>
2023-08-30 15:36:14 -0400

    finish

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-anonymous-default-export */
+import '../components/Button.css'
+
+export default  props =>{
+
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    
+
+    return(
+        <button
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
---
+++ src/components/Display.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+import '../components/Display.css'
+
+export default props =>{
+
+    return(
+    <div className='display'>{props.value}</div>
+
+    )
+}
